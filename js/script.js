09335e289844a363248a4702e39ee9d00a590c38
Barbarubia <dnl.desimone@gmail.com>
2022-02-23 10:50:34 -0300

    tentativo con switch FALLITO (messo nei commenti)

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ for (let i = 1; i <= 100; i++) {
     // attribuisco all'elemento div la classe box, definita nel css
     div.classList.add('box');
        
-    // condizione: i multiplo di 3 ma non multiplo di 5
+    //condizione: i multiplo di 3 ma non multiplo di 5
     if (i % 3 == 0 && i % 5 !== 0) {
         // aggiungo a questa condizione la classe yellow per cambiare il colore dello sfondo e la classe text-style per formattare il testo
         div.classList.add('yellow', 'text-style');
@@ -35,6 +35,45 @@ for (let i = 1; i <= 100; i++) {
         // stampo il valore di i
         div.innerHTML = i;
     }
+
+
+
+    /*
+    // TENTATIVO DI USARE SWITCH FALLITO
+    // Il problema dovrebbe essere nella definzione dei vari cases, che non vengono interpretati
+
+
+    switch (i) {
+        case (i % 3 == 0 && i % 5 !== 0):
+            // aggiungo a questa condizione la classe yellow per cambiare il colore dello sfondo e la classe text-style per formattare il testo
+            div.classList.add('yellow', 'text-style');
+            // stampo la scritta Fizz
+            div.innerHTML = `Fizz`;
+            break;
+
+        case (i % 5 == 0 && i % 3 !== 0):
+            // aggiungo a questa condizione la classe green per cambiare il colore dello sfondo e la classe text-style per formattare il testo
+            div.classList.add('green', 'text-style');
+            // stampo la scritta Buzz
+            div.innerHTML = `Buzz`;
+            break;
+
+        case (i % 3 == 0 && i % 5 == 0):
+            // aggiungo a questa condizione la classe cyan per cambiare il colore dello sfondo e la classe text-style per formattare il testo
+            div.classList.add('cyan', 'text-style');
+            // stampo la scritta FizzBuzz
+            div.innerHTML = `FizzBuzz`;
+            break;
+        
+        default:
+            // stampo il valore di i
+             div.innerHTML = i;
+            
+    }
+    */
+
+
+
     // Appendo l'elemento div creato all'interno del suo container
     areaContent.append(div);
 }
